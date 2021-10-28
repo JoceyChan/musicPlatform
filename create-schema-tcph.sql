89e0457411148ba28848b36a75932e9821e789fb
Jocey Chan <joceychan331718@gmail.com>
2021-10-28 13:24:05 -0700

    created nation table

--- create-schema-tcph.sql
+++ create-schema-tcph.sql
@@ -3,3 +3,9 @@ CREATE TABLE region (
     r_name char(25) not null,
     r_comment varchar(152)
 );
+CREATE TABLE nation (
+    n_nationkey decimal(3,0) not null,
+    n_name char(25) not null,
+    n_regionkey decimal(2,0) not null,
+    n_comment varchar(152)
+);
