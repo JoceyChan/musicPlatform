d640feb926a581316b093223f041a4df337c25a5
Jocey Chan <joceychan331718@gmail.com>
2021-10-28 13:25:37 -0700

    no comma

--- create-schema-tcph.sql
+++ create-schema-tcph.sql
@@ -1,6 +1,6 @@
 CREATE TABLE region (
     r_regionkey decimal(2,0) not null,
-    r_name char(25) not null,
+    r_name char(25) not null
 );
 CREATE TABLE nation (
     n_nationkey decimal(3,0) not null,
