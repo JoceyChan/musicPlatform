da3f71d1bb8a433ddbb0ff5b4e42e1e4559e5018
Christine Pham <phamchristine44.com>
2021-10-28 14:02:27 -0700

    create user

--- create-schema-tcph.sql
+++ create-schema-tcph.sql
@@ -7,3 +7,10 @@ CREATE TABLE nation (
     n_name char(25) not null,
     n_regionkey decimal(2,0) not null
 );
+CREATE TABLE user(
+    u_userkey decimal(3,0) not null, 
+    u_userName char(25) not null, 
+    u_passcode decimal(3,0) not null,
+    u_email char(25) not null,           -- char or #?
+    u_nationkey decimal(3,0) not null
+);
