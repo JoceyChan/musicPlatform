bbb7c867639a707eaa3f740c844c9c59d79afc38
Jocey Chan <joceychan331718@gmail.com>
2021-11-16 14:33:52 -0800

    Fixed query 2

--- queries.sql
+++ queries.sql
@@ -4,9 +4,13 @@ VALUES('happier', 'SOUR', 'Olivia Rodrigo', '2:55');
 
 -- 2 
 INSERT INTO user(u_userkey, u_userName, u_passcode, u_email, u_nationkey)
-VALUES(017, 'christBeanie', 1235, 'christineBeanie@gmail.com', 01);
+VALUES 
+(017, 'christBeanie', 1235, 'christineBeanie@gmail.com', 01),
+(018, 'jocey chan', 9429, 'joceychan331718@gmail.com', 01);
 
 -- 3
 UPDATE user
 SET u_userName = 'christineBeanie', u_passcode = 1230
 WHERE u_userkey = 017;
+
+-- 4 
