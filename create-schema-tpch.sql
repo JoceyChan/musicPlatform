792294fea5466e7b6708e8e9066e60483cc6a86e
Jocey Chan <joceychan331718@gmail.com>
2021-11-15 22:31:02 -0800

    changed to size of 2

--- create-schema-tpch.sql
+++ create-schema-tpch.sql
@@ -3,7 +3,7 @@ CREATE TABLE region (
     r_name char(25) not null
 );
 CREATE TABLE nation (
-    n_nationkey decimal(3,0) not null,
+    n_nationkey decimal(2,0) not null,
     n_name char(25) not null,
     n_regionkey decimal(2,0) not null
 );
