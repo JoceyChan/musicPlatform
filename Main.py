41b25073e4e7114627ed6f55ddb2616dacc9752e
Jocey Chan <joceychan331718@gmail.com>
2021-12-06 14:10:29 -0800

    openConnection File

--- Main.py
+++ Main.py
@@ -1,2 +1,17 @@
 import sqlite3
 from sqlite3 import Error
+
+def openConnection(_dbFile):
+    print("++++++++++++++++++++++++++++++++++")
+    print("Open database: ", _dbFile)
+
+    conn = None
+    try:
+        conn = sqlite3.connect(_dbFile)
+        print("success")
+    except Error as e:
+        print(e)
+
+    print("++++++++++++++++++++++++++++++++++")
+
+    return conn
