d7c08741855db25ddb4784f6a1f1ff979c664957
Jocey Chan <joceychan331718@gmail.com>
2021-12-07 12:47:31 -0800

    updated

--- create-schema-tpch.sql
+++ create-schema-tpch.sql
@@ -1,59 +1,59 @@
 SELECT "1----------";
 .headers on
 
-CREATE TABLE region (
+CREATE TABLE IF NOT EXISTS region (
     r_regionkey decimal(2,0) not null,
     r_name char(25) not null
 );
-CREATE TABLE nation (
+CREATE TABLE IF NOT EXISTS nation (
     n_nationkey decimal(2,0) not null,
     n_name char(25) not null,
     n_regionkey decimal(2,0) not null
 );
-CREATE TABLE user(
+CREATE TABLE IF NOT EXISTS user(
     u_userkey decimal(3,0) not null, 
     u_userName char(25) not null, 
     u_passcode decimal(4,0) not null,
     u_email char(25) not null,           -- char or #?
     u_nationkey decimal(3,0) not null
 );
-CREATE TABLE artist(
+CREATE TABLE IF NOT EXISTS artist(
     a_artistkey decimal(3,0) not null, 
     a_artistName char(25) not null, 
     a_passcode decimal(4,0) not null,
     a_email char(25) not null,           -- char or #?
     a_nationkey decimal(3,0) not null
 );
-CREATE TABLE music (
+CREATE TABLE IF NOT EXISTS music (
     m_songName char(25) not null,
     m_albumName char(25) not null,
     m_artistName char(25) not null,
-    m_duration char(25) not null,
-    m_key decimal(3,0) not null
+    m_duration char(25) not null
 );
-CREATE TABLE playlist (
+CREATE TABLE IF NOT EXISTS playlist (
     p_playlistName char(25) not null,
     p_playlistKey decimal(3, 0) not null, 
     p_songName char(25) not null, 
     p_artistName char(25) not null,
     p_userKey decimal(3, 0) not null
 );
-CREATE TABLE sharing(
+CREATE TABLE IF NOT EXISTS sharing(
     s_profileKey decimal (3,0) not null, 
     s_playlistName char(25) not null ,
     s_albumName char(25) not null, 
     s_songName char(25) not null
 );
-CREATE TABLE artistProfile(
+CREATE TABLE IF NOT EXISTS artistProfile(
     apr_profileKey decimal (2,0) not null, 
     apr_artistKey decimal (3,0) not null, 
     apr_nationkey decimal (3,0) not null
 );
-CREATE TABLE userProfile(
+CREATE TABLE IF NOT EXISTS userProfile(
     upr_profileKey decimal (2,0) not null, 
     upr_userKey decimal (3,0) not null, 
     upr_nationkey decimal (3,0) not null
 );
+
 DROP TABLE playlist;
 ;
 .headers off
--- database.py
+++ database.py
@@ -20,3 +20,53 @@ def close_db(e=None):
     db = g.pop('tpch', None)
     if db is not None:
         db.close()
+
+def get_profile(user, password):
+    try:
+        cur = get_db()
+        user_data = cur.execute("""SELECT u_userName,
+                                           u_email,
+                                           n_name
+                                      FROM user,
+                                           userProfile,
+                                           nation
+                                     WHERE upr_userKey = u_userkey AND 
+                                           upr_nationKey = n_nationKey AND 
+                                           u_userName = ? AND 
+                                           u_passcode = ?;
+                                    """, (user, password)).fetchone()
+        if user_data is None:
+            return None
+        else:
+            return user_data
+    except sqlite3.Error as error:
+        print(error)
+
+
+def get_all_songs():
+    try:
+        cur = get_db()
+        music_data = cur.execute("""SELECT * FROM music; """).fetchall()
+        if music_data is None:
+            return None
+        else:
+            return music_data
+    except sqlite3.Error as error:
+        print(error)
+
+
+def get_playlist_songs(playlist):
+    """
+    FIXME: Need to find a query to return the music in the selcted playlist name
+    :param playlist: playlist name in the database
+    :return: list of songs
+    """
+    try:
+        cur = get_db()
+        music_data = cur.execute("""SELECT * FROM music; """).fetchall()
+        if music_data is None:
+            return None
+        else:
+            return music_data
+    except sqlite3.Error as error:
+        print(error)
